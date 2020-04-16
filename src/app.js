65f2de9b492be446c818e9bbea0c4c425945f909
César Moreno Amado <camorenoa@yahoo.es>
2020-04-16 00:10:24 -0500

    Module 3

--- src/app.js
+++ src/app.js
@@ -12,6 +12,9 @@ app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 app.use(express.static(`${__dirname}/public`));
 
+app.use(express.urlencoded({ extended: true }));
+
+
 // Routes
 app.get('/', (req, res) => {
   res.render('index', {title: 'Account Summary', accounts: accounts});
@@ -33,6 +36,35 @@ app.get('/profile', (req, res) => {
   res.render('profile', {user: users[0]});
 });
 
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+  const amount = req.body.amount;
+  accounts[req.body.from].balance = accounts[req.body.from].balance - amount,10;
+  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(amount,10);
+  
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(`${__dirname}/json/accounts.json`, accountsJSON, 'utf8');
+  res.render('transfer', { message: "Transfer Completed" });
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+  accounts.credit.balance = accounts.credit.balance - req.body.amount;
+  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
+
+  const accountsJSON = JSON.stringify(accounts, null, 4);
+  fs.writeFileSync(`${__dirname}/json/accounts.json`, accountsJSON, 'utf8');
+  res.render('payment', { message: "Payment Successful", account: accounts.credit });
+});
+
+
+// Init server
 app.listen(3000, () => {
   console.log('server listen 3000 port');
 });
