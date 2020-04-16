41f3b3e5055d7c15ab793390f42362c83ef33947
César Moreno Amado <camorenoa@yahoo.es>
2020-04-16 18:44:42 -0500

    Module 5

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const {accounts, users, writeJSON} = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
@@ -10,52 +12,17 @@ app.use(express.static(`${__dirname}/public`));
 
 app.use(express.urlencoded({ extended: true }));
 
-
 // Routes
 app.get('/', (req, res) => {
   res.render('index', {title: 'Account Summary', accounts: accounts});
-});
-
-app.get('/savings', (req, res) => {
-  res.render('account', {account: accounts.savings});
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', {account: accounts.checking});
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', {account: accounts.credit});
 });
 
 app.get('/profile', (req, res) => {
   res.render('profile', {user: users[0]});
 });
 
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-
-app.post('/transfer', (req, res) => {
-  const amount = req.body.amount;
-  accounts[req.body.from].balance = accounts[req.body.from].balance - amount,10;
-  accounts[req.body.to].balance = parseInt(accounts[req.body.to].balance) + parseInt(amount,10);
-  
-  writeJSON();
-  res.render('transfer', { message: "Transfer Completed" });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', { account: accounts.credit });
-});
-
-app.post('/payment', (req, res) => {
-  accounts.credit.balance = accounts.credit.balance - req.body.amount;
-  accounts.credit.available = parseInt(accounts.credit.available) + parseInt(req.body.amount);
-
-  writeJSON();
-  res.render('payment', { message: "Payment Successful", account: accounts.credit });
-});
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 
 // Init server
