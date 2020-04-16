e9f04978d29e58a8f13fd471f4b4a544762ad8c4
César Moreno Amado <camorenoa@yahoo.es>
2020-04-15 22:25:11 -0500

    Module 1

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
+app.use(express.static(`${__dirname}/public`));
+
+app.get('/', (req, res) => {
+  res.render('index', {title: 'Index'});
+});
+
+app.listen(3000, () => {
+  console.log('server listen 3000 port');
+});
